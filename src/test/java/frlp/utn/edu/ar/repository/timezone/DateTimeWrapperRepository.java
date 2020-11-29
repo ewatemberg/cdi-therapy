bd9a95bce7bee29ac5faccddb8dfe3da728b59eb
Emilio Watemberg <ewatemberg.globallogic@supervielle.com.ar>
2020-11-29 11:45:00 -0300

    Se hizo rollback de webflux ya que en su version beta venia con bastantes bugs

---
+++ src/test/java/frlp/utn/edu/ar/repository/timezone/DateTimeWrapperRepository.java
@@ -0,0 +1,12 @@
+package frlp.utn.edu.ar.repository.timezone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the {@link DateTimeWrapper} entity.
+ */
+@Repository
+public interface DateTimeWrapperRepository extends JpaRepository<DateTimeWrapper, Long> {
+
+}
