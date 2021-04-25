0177c824cb1ce1e35a38ef663b4a680887e49f8f
Emilio Watemberg <emilio.watemberg@gmail.com>
2021-04-24 21:35:44 -0300

    Se agrego el titulo del cuestionario

---
+++ src/main/webapp/app/entities/cuestionario/cuestionario-load.tsx
@@ -0,0 +1,83 @@
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {Link, RouteComponentProps} from 'react-router-dom';
+import {Button, Row, Col} from 'reactstrap';
+import {Translate} from 'react-jhipster';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+import {IRootState} from 'app/shared/reducers';
+import {getEntity} from './cuestionario.reducer';
+import {APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT} from 'app/config/constants';
+
+export interface ICuestionarioDetailProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {
+}
+
+export const CuestionarioLoad = (props: ICuestionarioDetailProps) => {
+  useEffect(() => {
+    props.getEntity(props.match.params.id);
+  }, []);
+
+  const {cuestionarioEntity} = props;
+  return (
+    <div>
+      <Row>
+        <Col sm="12" md={{size: 6, offset: 3}}>
+          <div style={{textAlign: "center"}}>
+            <h2 data-cy="cuestionarioDetailsHeading" style={{marginBottom: 22}}>
+              <Translate contentKey="cdiApp.cuestionario.load.title">Inventario del Desarrollo Comunicativo
+                MacArthur-Bates</Translate>
+            </h2>
+            <h3>
+              <Translate contentKey="cdiApp.cuestionario.load.subtitle">Forma II Palabras y oraciones</Translate>
+            </h3>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="12">
+          {/*agregar la informacion del paciente*/}
+        </Col>
+      </Row>
+      <Row>
+        <Col md="12">
+          <dl className="jh-entity-details">
+            <dt>
+              <span id="id">
+                <Translate contentKey="global.field.id">ID</Translate>
+              </span>
+            </dt>
+            <dd>{cuestionarioEntity.id}</dd>
+            <dt>
+              <Translate contentKey="cdiApp.cuestionario.paciente">Paciente</Translate>
+            </dt>
+            <dd>{cuestionarioEntity.paciente ? cuestionarioEntity.paciente.id : ''}</dd>
+          </dl>
+          <Button tag={Link} to="/cuestionario" replace color="info" data-cy="entityDetailsBackButton">
+            <FontAwesomeIcon icon="arrow-left"/>{' '}
+            <span className="d-none d-md-inline">
+              <Translate contentKey="entity.action.back">Back</Translate>
+            </span>
+          </Button>
+          &nbsp;
+          <Button tag={Link} to={`/cuestionario/${cuestionarioEntity.id}/edit`} replace color="primary">
+            <FontAwesomeIcon icon="pencil-alt"/>{' '}
+            <span className="d-none d-md-inline">
+              <Translate contentKey="entity.action.edit">Edit</Translate>
+            </span>
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const mapStateToProps = ({cuestionario}: IRootState) => ({
+  cuestionarioEntity: cuestionario.entity,
+});
+
+const mapDispatchToProps = {getEntity};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(CuestionarioLoad);
--- src/main/webapp/app/entities/cuestionario/index.tsx
+++ src/main/webapp/app/entities/cuestionario/index.tsx
@@ -5,6 +5,7 @@ import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import Cuestionario from './cuestionario';
 import CuestionarioDetail from './cuestionario-detail';
+import CuestionarioLoad from './cuestionario-load';
 import CuestionarioUpdate from './cuestionario-update';
 import CuestionarioDeleteDialog from './cuestionario-delete-dialog';
 
@@ -14,6 +15,7 @@ const Routes = ({ match }) => (
       <ErrorBoundaryRoute exact path={`${match.url}/new`} component={CuestionarioUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={CuestionarioUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={CuestionarioDetail} />
+      <ErrorBoundaryRoute exact path={`${match.url}/:id/load`} component={CuestionarioLoad} />
       <ErrorBoundaryRoute path={match.url} component={Cuestionario} />
     </Switch>
     <ErrorBoundaryRoute exact path={`${match.url}/:id/delete`} component={CuestionarioDeleteDialog} />
